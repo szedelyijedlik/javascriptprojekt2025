efa4d87688896979997bdd812b2bbd977825076a
Szédelyi Ábris Péter <szedelyi.abris.peter@students.jedlik.eu>
2025-04-02 11:36:52 +0200

    v0.04 Plant osztály

--- game.js
+++ game.js
@@ -6,59 +6,63 @@ const ctx = canvas.getContext("2d");
 const width = canvas.width;
 const height = canvas.height;
 
+let plants = [];
+
 function drawBg() {
     const img = new Image();
     img.src = "test.webp";
     img.onload = function () {
         ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
 
-        Computer();
-    };
-}
-
-function drawComputerBg() {
-    ctx.fillStyle = "#013319";
-    let bgX = 425;
-    let bgY = 410;
-    let bgW = 305;
-    let bgH = 209;
-    ctx.fillRect(bgX, bgY, bgW, bgH);
-
-    ctx.fillStyle = "white";
-    ctx.textAlign = "center";
-    ctx.font = "15px Arial";
-    ctx.fillText("Weed Shop Management", width / 2 - 20, 430);
-
-    ctx.strokeStyle = "white";
-    ctx.moveTo(425, 440);
-    ctx.lineTo(730, 440);
-    ctx.stroke();
-
-    const img = new Image();
-    img.src = "monitor_done.png";
-    img.onload = function () {
-        let imgX = 245;
-        let imgY = 200;
-        // ctx.drawImage(img, imgX, imgY, imgX + 428, imgY + 311);
-    };
-}
-
-function Computer() {
-    drawComputerBg();
-
+        drawPlants();
+    }
 }
 
 function drawPlants() {
     const img2 = new Image();
     img2.src = "plant.webp";
     img2.onload = function () {
-        for (let i = 0; i < 6; i++) {
-            ctx.drawImage(img2, 285 + i * 100, 250, 70, 70);
-        }
-        for (let i = 0; i < 6; i++) {
-            ctx.drawImage(img2, 285 + i * 100, 320, 70, 70);
+        for (let index = 0; index < plants.length; index++) {
+            ctx.drawImage(img2, 285 + index * 60, 250, 70, 70);
         }
     };
 }
 
+function buyPlant(id = plants.length + 1, type = "Lemon Haze") {
+    plants.push(new Plant(id, type));
+    drawPlants();
+}
+
+window.addEventListener("click", function (event) {
+    const rect = canvas.getBoundingClientRect();
+    const scaleX = canvas.width / rect.width;
+    const scaleY = canvas.height / rect.height;
+    
+    const x = (event.clientX - rect.left) * scaleX;
+    const y = (event.clientY - rect.top) * scaleY;
+
+    for (let index = 0; index < plants.length; index++) {
+        if (x > 285 + index * 60 && x < 285 + index * 60 + 70 && y > 250 && y < 250 + 70) {
+            ToolTip(plants[index]);
+        }
+    }
+});
+
+function ToolTip(plant) {
+    ctx.fillStyle = "#013319";
+    ctx.fillRect(426, 410, 304, 209);
+
+    const img2 = new Image();
+    img2.src = "plant.webp";
+    img2.onload = function () {
+            ctx.drawImage(img2, 460, 460, 70, 70);
+    };
+}
+
 drawBg();
+buyPlant();
+buyPlant();
+buyPlant();
+buyPlant();
+buyPlant();
+buyPlant();
