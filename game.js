47f42a431083be1f1028da2ed63ba75cc5b14a8a
Szédelyi Ábris Péter <szedelyi.abris.peter@students.jedlik.eu>
2025-04-09 11:52:51 +0200

    v0.2 game changes

--- game.js
+++ game.js
@@ -7,44 +7,64 @@ const ctx = canvas.getContext("2d");
 const width = canvas.width;
 const height = canvas.height;
 
-const shop = new Shop();
-
 let plants = [];
+let shop = new Shop();
+
 const plantTypes = [
     ["Lemon Haze", 10, 15, "images/lemonhaze.webp"],
-    ["Grey Haze", 10, 15, "images/lemonhaze.webp"],
-    ["Blueberry Haze", 10, 15, "images/lemonhaze.webp"],
-    ["Alban Haze", 10, 15, "images/lemonhaze.webp"],
-    ["Strawberry Haze", 10, 15, "images/lemonhaze.webp"],
-    ["Orange Haze", 10, 15, "images/lemonhaze.webp"]
+    ["Ghost Haze", 10, 15, "images/ghosthaze.png"],
+    ["Albán szamár", 10, 15, "images/albanszamar.png"],
+    ["Octopu$$y", 10, 15, "images/octopu$$y.png"],
+    ["Party in the parlament", 10, 15, "images/party_in_the_parlament.png"],
+    ["Strain of death", 10, 15, "images/strain_of_death.png"]
 ];
 
+if (localStorage.getItem("plants") != null && localStorage.getItem("plants") != "") {
+    plants = JSON.parse(localStorage.getItem("plants"));
+}
+
+if (localStorage.getItem("shop") != null && localStorage.getItem("shop") != "") {
+    shop = JSON.parse(localStorage.getItem("shop"));
+}
 
 function drawPlants() {
+    ctx.clearRect(0, 0, width, height);
+    const plantsPerRow = 9;
     for (let index = 0; index < plants.length; index++) {
+        const row = Math.floor(index / plantsPerRow);
+        const col = index % plantsPerRow;
+
+        const x = 281 + col * 64;
+        const y = 255 + row * 62;
+
         const img = new Image();
         img.src = plants[index].image;
         img.onload = function () {
             if (plants[index].water < 10) {
                 ctx.beginPath();
                 ctx.fillStyle = "lightblue";
-                ctx.roundRect(316 + index * 60, 230, 10, 10, 150);
+                ctx.roundRect(x + 30, y - 20 , 10, 10, 150);
                 ctx.fill();
             }
             if (plants[index].status == 100) {
                 ctx.beginPath();
                 ctx.fillStyle = "lightgreen";
-                ctx.roundRect(318 + index * 60, 230, 10, 10, 150);
+                ctx.roundRect(x + 30, y - 20, 10, 10, 150);
                 ctx.fill();
             }
-            ctx.drawImage(img, 285 + index * 60, 250, 70 + plants[index].status / 20, 70 + plants[index].status / 20);
+            ctx.drawImage(img, x, y, 68, 68);
         }
-    };
+    }
 }
 
-function buyPlant(type = "Lemon Haze") {
-    plants.push(new Plant(type));
-    drawPlants();
+function buyPlant(type) {
+    if (plants.length < 18) {
+        plants.push(new Plant(type));
+        drawPlants();
+    }
+    else {
+        alert("Sikertelen művelet: Nincs több hely a polcon");
+    }
 }
 
 window.addEventListener("click", function (event) {
@@ -52,14 +72,22 @@ window.addEventListener("click", function (event) {
     const scaleX = canvas.width / rect.width;
     const scaleY = canvas.height / rect.height;
     
-    const x = (event.clientX - rect.left) * scaleX;
-    const y = (event.clientY - rect.top) * scaleY;
+    const cX = (event.clientX - rect.left) * scaleX;
+    const cY = (event.clientY - rect.top) * scaleY;
 
+    const plantsPerRow = 9;
     for (let index = 0; index < plants.length; index++) {
-        if (x > 290 + index * 60 && x < 285 + index * 60 + 65 && y > 250 && y < 250 + 70) {
+        const row = Math.floor(index / plantsPerRow);
+        const col = index % plantsPerRow;
+
+        const x = 281 + col * 64;
+        const y = 255 + row * 62;
+
+        if (cX + 1 > x && cX < x + 63 && cY > y && cY < y + 60) {
             ToolTip(plants[index]);
         }
     }
+
 });
 
 function ToolTip(plant) {
@@ -97,7 +125,7 @@ function ToolTip(plant) {
         ctx.fillText("Szüretelés", 508, 595);
         ctx.fillText("Öntözés", 593, 595);
 
-        window.addEventListener("click", function (event) {
+        tooltiplistener = window.addEventListener("click", function (event) {
             const rect = canvas.getBoundingClientRect();
             const scaleX = canvas.width / rect.width;
             const scaleY = canvas.height / rect.height;
@@ -108,13 +136,36 @@ function ToolTip(plant) {
             if (x > 500 && x < 570 && y > 580 && y < 605) {
                 shop.Harvest(plant);
                 plants.splice(plants.indexOf(plant), 1);
+                ctx.clearRect(426, 410, 304, 209);
+                drawPlants();
+                console.log("Harvested plant");
             }
         });
     };
 
     timeout = setTimeout(() => {
         ctx.clearRect(426, 410, 304, 209);
+        removeEventListener("click", tooltiplistener);
+        tooltiplistener = null;
     }, 5000);
+}
+
+function alert(msg) {
+    let x = width / 2;
+    let y = 100;
+
+    ctx.clearRect(0, 0, width, 150);
+    ctx.fillStyle = "darkred";
+    ctx.roundRect(x - msg.length * 4, y, msg.length * 8, 40, 5);
+    ctx.fill();
+    ctx.fillStyle = "white";
+    ctx.font = "15px Arial";
+    ctx.textAlign = "center";
+    ctx.fillText(msg, x, y + 25);
+
+    setTimeout(() => {
+        ctx.clearRect(0, 0, width, 150);
+    }, 2000);
 }
 
 setInterval(() => {
@@ -135,10 +186,5 @@ setInterval(() => {
 }, 2000);
 
 let timeout = null;
+let tooltiplistener = null;
 drawPlants();
-buyPlant(plantTypes[Math.floor(Math.random() * plantTypes.length)]);
-buyPlant(plantTypes[Math.floor(Math.random() * plantTypes.length)]);
-buyPlant(plantTypes[Math.floor(Math.random() * plantTypes.length)]);
-buyPlant(plantTypes[Math.floor(Math.random() * plantTypes.length)]);
-buyPlant(plantTypes[Math.floor(Math.random() * plantTypes.length)]);
-buyPlant(plantTypes[Math.floor(Math.random() * plantTypes.length)]);
